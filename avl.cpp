315544e0592fb9f2cb1ce3eeb2df2b0e6a649763
vstatnyk <62860849+vstatnyk@users.noreply.github.com>
2021-11-25 21:44:58 -0800

    avl now works and started work on printing

--- avl.cpp
+++ avl.cpp
@@ -4,8 +4,8 @@ using namespace std;
 struct node
 {
     int key;
-    node* left;
-    node* right;
+    struct node* left;
+    struct node* right;
     int height;
 };
 
@@ -20,7 +20,7 @@ int height(node *node)
 
 node *newNode(int key)
 {
-    node* nn = new node;
+    struct node* nn = new node;
     nn->key = key;
     nn->height = 0;
     nn->left = NULL;
@@ -43,73 +43,33 @@ int getBalance(node *node)
     return(leftHeight - rightHeight);
 }
 
-void printLevel(node *node)
-{
-
-}
-
 node *leftRotate(node *x){
-   struct node *y = x->right;
-   //add more code to rotate to the left, update heights for x and y
-   y->left = x;
-   x->right = NULL;
-   y->height ++;
-   x->height --;  
-   return(y);
+    struct node *y = x->right;
+    //add more code to rotate to the left, update heights for x and y
+    y->left = x;
+    x->right = NULL;
+    y->height = max(height(x->left), height(x->right)) + 1;
+    x->height --;
+    if(y->right != NULL)
+    {
+        y->right->height ++;
+    }  
+    return(y);
 }
 
 node *rightRotate(node *x){
-   struct node *y=x->left;
-   //add more code to rotate to the right, update heights for x and y
-   y->right = x;
-   x->left = NULL;
-   y->height ++;
-   x->height --;  
-   return(y);
+    struct node *y=x->left;
+    //add more code to rotate to the right, update heights for x and y
+    y->right = x;
+    x->left = NULL;
+    y->height = max(height(x->left), height(x->right)) + 1;
+    x->height --;
+    if(y->left != NULL)
+    {
+        y->left->height ++;
+    }
+    return(y);
 }
-// int max( node *L, node *R){
-
-//       if (L == NULL && R == NULL){
-//         return -1;
-//       }
-//       else if (L == NULL){
-//             return (R->height);
-//       }
-//       else{
-
-//         return (L->height);
-//       }
-// }
-// node *leftRotate( node *x){
-
-//     node *y=x->right;
-
-//     // add more code to rotate to the left,
-//     x->right=y->left;
-//     y->left=x;
-
-//     // update heights for x and y
-//     x->height = (max(x->left, x->right)) + 1;
-//     y->height = (max(y->left, y->right)) + 1;
-
-//    //return root of the tree
-//     return y;
-// }
-
-// node *rightRotate( node *x){
-
-//     node *y=x->left;
-//    //add more code to rotate to the right,
-//     x->left=y->right;
-//     y->right=x;
-
-//     //   update heights for x and y
-//     x->height = (max(x->left, x->right)) + 1;
-//     y->height = (max(y->left, y->right)) + 1;
-
-//     //   return root of the tree
-//     return y;
-// }
 
 node * rebalance(node *node){
     node->height = max(height(node->left), height(node->right)) + 1;    
@@ -148,7 +108,8 @@ node* insert(node*& node, int key)
     //When insert happens set height to 0 for the node     
     if (node == NULL)
     {
-        return(newNode(key));
+        node = newNode(key);
+        return(node);
     }
     if (key < node->key)
     {
@@ -162,20 +123,48 @@ node* insert(node*& node, int key)
     return node;
 }
 
+void printLevel(node *node)
+{
+    struct node *endlevel = NULL;
+
+}
+
+void push(node *node)
+{
+
+}
+
+void pop(node *node)
+{
+
+}
+
+// Enqueue root ( NodesOnCurrentLevel++)   // add node in current level
+// While NodesinCurrentLevel!=0                //or nodesInCurrentLevel !=0
+// Dequeue and display //1st time (A)
+// nodesInCurrentLevel--   //1st time dequeue A,NICL=0 
+// enqueue left  child( nodesInNextLevel++)   //1st time enqueue B
+// enqueue right child ( nodesInNextLevel++)   //1st time enqueue C
+// If nodesInCurrentLevel==0 //1st time NICL=0
+// NodesInCurrentlevel=NodesInNextLevel  //points to B C
+// NodeinNextlevel=0 //Points to nothing
+// cout<<endl //go to next line
+
 int main()
 {
-    node *root = NULL;
-    root = insert(root, 9);
-    cout << root->key;
+    struct node *root = NULL;
+    insert(root, 3);
+    // cout << root->key;
+    // cout << root->height << '\n';
+    insert(root,5);
+    // cout << root->left->key;
+    // cout << root->left->height<<'\n';
+    insert(root, 7);
+    cout << root->key << " ";
     cout << root->height << '\n';
-    root = insert(root, 5);
-    cout << root->left->key;
+    cout << root->left->key<< " ";
     cout << root->left->height<<'\n';
-    root = insert(root, 3);
-    cout << root->key;
-    cout << root->height << '\n';
-    cout << root->left->key;
-    cout << root->left->height<<'\n';
-    cout << root->right->key;
+    cout << root->right->key<< " ";
     cout << root->right->height<<'\n';
+    printLevel(root);
 }
